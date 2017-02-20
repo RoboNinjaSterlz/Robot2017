6124e84832165cda8008138d9e0ea7203aa16356
team2016 <team2016@0f5244a1-5b54-49b8-b685-b57abccc5b47>
2017-02-20 20:41:28 +0000

    Fixed motor directions and speeds, added comp for low goal ball intake.

--- src/org/usfirst/frc2016/robot2017/subsystems/LowGoalShooter.java
+++ src/org/usfirst/frc2016/robot2017/subsystems/LowGoalShooter.java
@@ -36,10 +36,10 @@ public class LowGoalShooter extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public void lowShootIn() {
-    	lowGoalSpark.set(-1);
+    	lowGoalSpark.set(1);
     }
     public void lowShootOut() {
-    	lowGoalSpark.set(1);
+    	lowGoalSpark.set(-1);
     }
     public void lowShootOff() {
     	lowGoalSpark.set(0);
