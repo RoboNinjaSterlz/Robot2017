5be06c89b57ed3d7ebd283f7bbd4bdc1c8dfdc5f
sterly4 <sterly4@0f5244a1-5b54-49b8-b685-b57abccc5b47>
2017-03-16 02:11:04 +0000

    fixed auto issues added auto right gear

--- trunk/src/org/usfirst/frc2016/robot2017/subsystems/GearElevator.java
+++ trunk/src/org/usfirst/frc2016/robot2017/subsystems/GearElevator.java
@@ -111,7 +111,7 @@ public class GearElevator extends Subsystem {
     		needsCalibrate = false;
     	} else {
     		needsCalibrate = true;
-    		gearElevatorTalon.set(-.35); // Run back at 35% power
+    		gearElevatorTalon.set(-.45); // Run back at 45% power
     	}
     }
 
