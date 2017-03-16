740a4712ac7a28e4c4f69c02e3bd9c6707b2ef12
Sterly Deracy <25871926+RoboNinjaSterlz@users.noreply.github.com>
2017-03-16 02:09:12 +0000

    fixed auto issues added auto right gear

--- src/org/usfirst/frc2016/robot2017/RobotMap.java
+++ src/org/usfirst/frc2016/robot2017/RobotMap.java
@@ -78,11 +78,11 @@ public class RobotMap {
         drivetrainRobotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
         drivetrainLeftEncoder = new Encoder(0, 1, false, EncodingType.k4X);
         LiveWindow.addSensor("Drivetrain", "LeftEncoder", drivetrainLeftEncoder);
-        drivetrainLeftEncoder.setDistancePerPulse(0.045842);
+        drivetrainLeftEncoder.setDistancePerPulse(0.0386214295);
         drivetrainLeftEncoder.setPIDSourceType(PIDSourceType.kRate);
         drivetrainRightEncoder = new Encoder(2, 3, false, EncodingType.k4X);
         LiveWindow.addSensor("Drivetrain", "RightEncoder", drivetrainRightEncoder);
-        drivetrainRightEncoder.setDistancePerPulse(0.0397304);
+        drivetrainRightEncoder.setDistancePerPulse(0.0386214295);
         drivetrainRightEncoder.setPIDSourceType(PIDSourceType.kRate);
         drivetrainShiftSolenoid = new Solenoid(0, 0);
         LiveWindow.addActuator("Drivetrain", "ShiftSolenoid", drivetrainShiftSolenoid);
