68532a2978d497339bfb9661e74b0138a38e76e6
team2016 <team2016@0f5244a1-5b54-49b8-b685-b57abccc5b47>
2017-03-18 17:37:13 +0000

    Added code for plunger time to extend and added comments

--- trunk/src/org/usfirst/frc2016/robot2017/GearInTrigger.java
+++ trunk/src/org/usfirst/frc2016/robot2017/GearInTrigger.java
@@ -6,27 +6,40 @@ import org.usfirst.frc2016.robot2017.RobotMap;
 
 public class GearInTrigger extends Button {
 	private int activeCount;
+	private int solenoidActiveCount;
+	
 	GearInTrigger() {
-		activeCount = 0;
+	activeCount = 0;
+	solenoidActiveCount = 0;
 	}
 
 
 	public boolean get() {
 		boolean state;
-
+		// Count so we don't trigger on the first detection
 		if (!RobotMap.gearInDetector.get()) {
 			activeCount++;
 		}
 		else {
 			activeCount = 0;
 		}
+		// If this is re-run in auto, make sure the plunger had time to extend before
+		// trying again
+		if (!Robot.gearGrabber.isGrabbed()) {
+			solenoidActiveCount ++;
+		)
+		else {
+			solenoidActiveCount = 0;
+		}
+		
 		/* 
 		 * Check where the elevator is
 		 * If it isn't in prepare to grab, ignore the switch
 		 */
 		if (Robot.gearElevator.currentPreset() == 1 &&
-				Robot.oi.cCI.getRawButton(1) &&
-				!Robot.gearGrabber.isGrabbed()) {
+				Robot.oi.cCI.getRawButton(1) &&  // Must be in auto mode
+				!Robot.gearGrabber.isGrabbed() &&
+				solenoidActiveCount > 12) {   // Make sure grabber had time to open 12 should be about.25 seconds.
 			// Must be there for 3 counts before we say good.
 			return activeCount>2;
 		}
